8a3c459fbcde905e873f88734690d6a280c85889
Timofey335 <timofeyvalerievich+git@gmail.com>
2024-08-12 00:06:08 +0300

    added docker-compose containers(psql, migrator) and migrations

--- migrations/20240808_create_users_table.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-create table users (
-    chat_id int references chats(id) on delete cascade,
-    user_id int not null,
-    primary key (chat_id, user_id)
-);
-
--- +goose Down
-drop table users;
---
+++ migrations/20240811001811_create_chat_tables.sql
@@ -0,0 +1,33 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    role INT NOT NULL
+);
+
+CREATE TABLE chats (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    chat_id INT REFERENCES chats(id) ON DELETE CASCADE,
+    text TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE user_to_chats (
+    chat_id INT REFERENCES chats(id),
+    user_id INT REFERENCES users(id),
+    CONSTRAINT user_to_chats_pk PRIMARY KEY (chat_id, user_id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table users, chats, messages, user_to_chats;
+-- +goose StatementEnd
