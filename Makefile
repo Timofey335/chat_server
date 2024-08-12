723baa8cddb215e2fa05c451f5912d57584d3999
Timofey335 <timofeyvalerievich+git@gmail.com>
2024-08-13 00:23:55 +0300

    added the ability to add information to the database for all methods in main.go

--- Makefile
+++ Makefile
@@ -19,14 +19,13 @@ generate:
 	make generate-note-api
 
 generate-note-api:
-	mkdir -p pkg/authservice_v1
-	protoc --proto_path api/authservice_v1 \
-	--go_out=pkg/authservice_v1 --go_opt=paths=source_relative \
+	mkdir -p pkg/chat_server_v1
+	protoc --proto_path api/chat_server_v1 \
+	--go_out=pkg/chat_server_v1 --go_opt=paths=source_relative \
 	--plugin=protoc-gen-go=bin/protoc-gen-go \
-	--go-grpc_out=pkg/authservice_v1 --go-grpc_opt=paths=source_relative \
+	--go-grpc_out=pkg/chat_server_v1 --go-grpc_opt=paths=source_relative \
 	--plugin=protoc-gen-go-grpc=bin/protoc-gen-go-grpc \
-	--experimental_allow_proto3_optional \
-	api/authservice_v1/authservice.proto
+	api/chat_server_v1/chat_server.proto
 
 local-migration-status:
 	${LOCAL_BIN}/goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} status -v
--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,8 @@ import (
 	"net"
 	"time"
 
-	"github.com/brianvoe/gofakeit"
 	"github.com/fatih/color"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -16,13 +16,25 @@ import (
 	desc "github.com/Timofey335/chat-server/pkg/chat_server_v1"
 )
 
-const grpcPort = 50051
+const (
+	grpcPort = 50051
+	dbDSN    = "host=localhost port=54321 dbname=chat user=user password=userpassword sslmode=disable"
+)
 
 type server struct {
 	desc.UnimplementedChatServerV1Server
+	pool *pgxpool.Pool
 }
 
 func main() {
+	ctx := context.Background()
+
+	pool, err := pgxpool.Connect(ctx, dbDSN)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
 	if err != nil {
 		log.Fatalf(color.RedString("failed listen: %v", err))
@@ -30,7 +42,7 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterChatServerV1Server(s, &server{})
+	desc.RegisterChatServerV1Server(s, &server{pool: pool})
 	log.Println(color.BlueString("server listening at %v", lis.Addr()))
 	if err := s.Serve(lis); err == nil {
 		log.Fatalf(color.RedString("failed to serve: %v", err))
@@ -39,16 +51,29 @@ func main() {
 
 // CreateChat - create a new chat
 func (s *server) CreateChat(ctx context.Context, req *desc.CreateChatRequest) (*desc.CreateChatResponse, error) {
-	log.Println(color.BlueString("Create chat: %v, with ctx: %v", req.Usernames, ctx))
+	var chatId int64
+	err := s.pool.QueryRow(ctx, `INSERT INTO chats (name, users) VALUES ($1, $2) RETURNING id;`, req.Chatname, req.Usernames).Scan(&chatId)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println(color.BlueString("create chat: %v, with ctx: %v", req.Chatname, ctx))
 
 	return &desc.CreateChatResponse{
-		Id: gofakeit.Int64(),
+		Id: chatId,
 	}, nil
 }
 
 // DeleteChat - delete the chat by id
 func (s *server) DeleteChat(ctx context.Context, req *desc.DeleteChatRequest) (*emptypb.Empty, error) {
-	log.Println(color.HiMagentaString("Delete chat: id %d, with ctx: %v", req.Id, ctx))
+	var chatId int64
+	err := s.pool.QueryRow(ctx, `DELETE FROM chats WHERE id = $1 RETURNING id;`, req.Id).Scan(&chatId)
+	if err != nil {
+		log.Println(color.HiMagentaString("error while deleting the chat: %v, with ctx: %v", err, ctx))
+		return nil, err
+	}
+
+	log.Println(color.HiMagentaString("deleted chat: id %d, with ctx: %v", req.Id, ctx))
 
 	return &emptypb.Empty{}, nil
 }
@@ -56,7 +81,13 @@ func (s *server) DeleteChat(ctx context.Context, req *desc.DeleteChatRequest) (*
 // SendMessage - send message to the server
 func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
 	timestamp := req.Timestamp.AsTime().Format(time.UnixDate)
-	log.Println(color.BlueString("Send message: %s to %s at %s, with ctx: %v", req.Text, req.From, timestamp, ctx))
+	_, err := s.pool.Exec(ctx, `INSERT INTO messages (user_id, chat_id, text, created_at) VALUES ($1, $2, $3, $4);`, req.FromId, req.ChatId, req.Text, timestamp)
+	if err != nil {
+		log.Println(color.HiMagentaString("error while sending the message: %v, with ctx: %v", err, ctx))
+		return nil, err
+	}
+
+	log.Println(color.BlueString("sent message: %s from user id %s at %s, with ctx: %v", req.Text, req.FromId, timestamp, ctx))
 
 	return &emptypb.Empty{}, nil
 }
