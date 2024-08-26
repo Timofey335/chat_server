ac196842d817a86ed3c4127831cb7cc619ed3eb6
Timofey335 <timofeyvalerievich+git@gmail.com>
2024-08-27 00:14:25 +0300

    added squirrel to repo methods

--- internal/repository/chat/repository.go
+++ internal/repository/chat/repository.go
@@ -4,12 +4,28 @@ import (
 	"context"
 	"log"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/fatih/color"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/Timofey335/chat-server/internal/model"
 	"github.com/Timofey335/chat-server/internal/repository"
+)
+
+const (
+	tableChats = "chats"
+
+	idColumn    = "id"
+	nameColumn  = "name"
+	usersColumn = "users"
+
+	tableMessages = "messages"
+
+	userIdColumn    = "user_id"
+	chatIdColumn    = "chat_id"
+	textColumn      = "text"
+	createdAtColumn = "created_at"
 )
 
 type repo struct {
@@ -24,8 +40,18 @@ func NewChat(db *pgxpool.Pool) repository.ChatRepository {
 func (r *repo) CreateChat(ctx context.Context, chat *model.Chat) (int64, error) {
 	var chatId int64
 
-	err := r.db.QueryRow(ctx, `INSERT INTO chats (name, users) VALUES ($1, $2) RETURNING id;`, chat.Name, chat.Users).Scan(&chatId)
+	builderInsert := sq.Insert(tableChats).
+		PlaceholderFormat(sq.Dollar).
+		Columns(nameColumn, usersColumn).
+		Values(chat.Name, chat.Users).
+		Suffix("RETURNING id")
+
+	query, args, err := builderInsert.ToSql()
 	if err != nil {
+		log.Fatalf("failed to build query: %v", err)
+	}
+
+	if err = r.db.QueryRow(ctx, query, args...).Scan(&chatId); err != nil {
 		return 0, err
 	}
 
@@ -38,20 +64,40 @@ func (r *repo) CreateChat(ctx context.Context, chat *model.Chat) (int64, error) 
 func (r *repo) DeleteChat(ctx context.Context, chatId int64) (*emptypb.Empty, error) {
 	var id int64
 
-	err := r.db.QueryRow(ctx, `DELETE FROM chats WHERE id = $1 RETURNING id;`, chatId).Scan(&id)
+	builderDelete := sq.Delete(tableChats).
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{idColumn: chatId}).
+		Suffix("RETURNING id")
+
+	query, args, err := builderDelete.ToSql()
+	if err != nil {
+		log.Fatalf("failed to build query: %v", err)
+	}
+
+	err = r.db.QueryRow(ctx, query, args...).Scan(&id)
 	if err != nil {
 		log.Println(color.HiMagentaString("error while deleting the chat: %v, with ctx: %v", err, ctx))
 		return nil, err
 	}
 
-	log.Println(color.HiMagentaString("deleted chat: id %d, with ctx: %v", chatId, ctx))
+	log.Println(color.HiMagentaString("deleted chat: id %d, with ctx: %v", id, ctx))
 
 	return &emptypb.Empty{}, nil
 }
 
 // SendMessage - send message to the server
 func (r *repo) SendMessage(ctx context.Context, message *model.Message) (*emptypb.Empty, error) {
-	_, err := r.db.Exec(ctx, `INSERT INTO messages (user_id, chat_id, text, created_at) VALUES ($1, $2, $3, $4);`, message.UserId, message.ChatId, message.Text, message.CreatedAt)
+	builderInsert := sq.Insert(tableMessages).
+		PlaceholderFormat(sq.Dollar).
+		Columns(userIdColumn, chatIdColumn, textColumn, createdAtColumn).
+		Values(message.UserId, message.ChatId, message.Text, message.CreatedAt)
+
+	query, args, err := builderInsert.ToSql()
+	if err != nil {
+		log.Fatalf("failed to build query: %v", err)
+	}
+
+	_, err = r.db.Exec(ctx, query, args...)
 	if err != nil {
 		log.Println(color.HiMagentaString("error while sending the message: %v, with ctx: %v", err, ctx))
 		return nil, err
